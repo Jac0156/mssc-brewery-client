d2b7f4612bd460f6783a8d054e95fbe6d7765f5c
Jack Weby <jac0156@hotmail.com>
2024-09-05 17:36:07 -0400

    Ajouter test getBeerById

---
+++ src/test/java/guru/springframework/mssc_brewery_client/web/client/BreweryClientTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.mssc_brewery_client.web.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import guru.springframework.mssc_brewery_client.web.model.BeerDto;
+
+@SpringBootTest
+public class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+}
