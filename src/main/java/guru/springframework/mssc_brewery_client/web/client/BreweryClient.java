8646042feaa07befc17e04581b04d9ed63e3540f
Jack Weby <jac0156@hotmail.com>
2024-09-06 15:15:24 -0400

    Ajouter Save, Update, Delete BreweryClient

--- src/main/java/guru/springframework/mssc_brewery_client/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/mssc_brewery_client/web/client/BreweryClient.java
@@ -1,5 +1,6 @@
 package guru.springframework.mssc_brewery_client.web.client;
 
+import java.net.URI;
 import java.util.UUID;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -23,13 +24,22 @@ public class BreweryClient {
     }
 
     public BeerDto getBeerById(UUID uuid) {
-        return restTemplate.getForObject(apihost + BEER_PATH_V1 + "/" + uuid.toString(), BeerDto.class);
+        return restTemplate.getForObject(apihost + BEER_PATH_V1  + "/" + uuid.toString(), BeerDto.class);
     }
+
+    public URI saveNewBeer(BeerDto beerDto){
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
+    public void updateBeer(UUID uuid, BeerDto beerDto) {
+        restTemplate.put(apihost + BEER_PATH_V1  + "/" + uuid.toString(), beerDto);
+    }
+
+    public void deleteBeer(UUID uuid){
+        restTemplate.delete(apihost + BEER_PATH_V1 + "/" + uuid.toString());
+    } 
 
     public void setApihost(String apihost) {
         this.apihost = apihost;
     }
-
-
-
 }
--- src/test/java/guru/springframework/mssc_brewery_client/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/mssc_brewery_client/web/client/BreweryClientTest.java
@@ -2,6 +2,7 @@ package guru.springframework.mssc_brewery_client.web.client;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.URI;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -17,9 +18,29 @@ public class BreweryClientTest {
     BreweryClient client;
 
     @Test
-    void getBeerById() {
+    void testgetBeerById() {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
         assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        
+        BeerDto beerDto = BeerDto.builder().beerName("New beer").build();
+        URI uri = client.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("Upd beer").build();
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void testDeleteBeer() {
+        client.deleteBeer(UUID.randomUUID());
     }
 
 }
