d6ca9f9138139f6863ee79321df9d604480ccc16
Jack Weby <jac0156@hotmail.com>
2024-09-06 22:30:11 -0400

    Ajouter Apache HTTP Client config.

---
+++ src/main/java/guru/springframework/mssc_brewery_client/web/config/BlockingRestTemplateCustomizer.java
@@ -0,0 +1,50 @@
+package guru.springframework.mssc_brewery_client.web.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.DefaultConnectionKeepAliveStrategy;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.http.io.SocketConfig;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer{
+
+    public ClientHttpRequestFactory clientHttpRequestFactory(){
+        
+        SocketConfig socketConfig = SocketConfig.custom()
+                .setSoTimeout(3000, TimeUnit.MILLISECONDS)
+                .build();
+        
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setDefaultSocketConfig(socketConfig);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(3000, TimeUnit.MILLISECONDS)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    }
+}
